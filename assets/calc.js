1f185edc456b1be257c6e4ff11c891f2044d0936
AltDrk <hidn@protonmail.com>
2019-06-03 16:04:51 +0200

    oneliner

---
+++ assets/calc.js
@@ -0,0 +1,18 @@
+
+let $input1 = $('#input1')
+let $input2 = $('#input2')
+let $input3 = $('#input3')
+let $result = $('#result')
+
+let a = Number($input1[0].value);
+let b = Number($input2[0].value);
+let $op = $input3[0].value;
+let $result = $result[0].value;
+
+
+
+$result[0].value = x $op y;
+
+
+
+if $op == +  alors result = x + y
